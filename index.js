56c501e03997613526dae1bb4d897279087dc842
Matt Millar <matthew.millar@madetech.com>
2019-11-19 14:34:55 +0000

    Updated index.js to do the work

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function doSomething(callback) {
-  callback('response');
+require('dotenv').config();
+
+const UHTGateway = require('./lib/gateways/UHTGateway');
+const FetchHousingRegisterData = require('./lib/use_cases/FetchHousingRegisterData');
+
+// load sql queries
+require('./lib/sql').load();
+
+const FetchHousingRegisterDataUseCase = new FetchHousingRegisterData({
+  uhtGateway: new UHTGateway()
+});
+
+async function fetchData(biddingNumber, callback) {
+  let params = {};
+  if (biddingNumber) {
+    params.biddingNumber = biddingNumber;
+  }
+  callback(await FetchHousingRegisterDataUseCase.execute(params));
 }
 
 exports.handler = (event, context, callback) => {
-  doSomething(callback);
+  fetchData(event, callback);
 };
