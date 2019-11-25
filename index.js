9ad56b1b102a6fb755fad22dbe9c5ec840f1485c
Matt Millar <matthew.millar@madetech.com>
2019-11-25 10:25:12 +0000

    serverless updates

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const port = 3000;
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 const UHTGateway = require('./lib/gateways/UHTGateway');
 const FetchHousingRegisterData = require('./lib/use_cases/FetchHousingRegisterData');
@@ -16,12 +16,23 @@ const FetchHousingRegisterDataUseCase = new FetchHousingRegisterData({
   uhtGateway: new UHTGateway()
 });
 
+app.use(function(req, res, next) {
+  // had to rewrite the path to get it playing nice with a not-root resource in api gateway
+  req.url = req.url.replace('/hn-infoevidence', '');
+  next();
+});
+
 app.get('/housing_register', async (req, res) => {
+  console.log('Received request at "/housing_register"');
   let params = {};
   if (req.query.biddingNumber) {
     params.biddingNumber = req.query.biddingNumber;
   }
-  res.send(await FetchHousingRegisterDataUseCase.execute(params));
+  console.log('Fetching data...');
+  const response = await FetchHousingRegisterDataUseCase.execute(params);
+  console.log('Finished fetching data.');
+  console.log(JSON.stringify(response));
+  res.send(response);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
